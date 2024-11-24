406f681e46a1a94edb261129854860c58aa220c8
flowmode <17271740+flowmode@users.noreply.github.com>
2024-11-24 15:28:46 +0700

    chore: update SDKs for version 0.0.2

--- wink-sdk-affiliate-winklinks/wink_sdk_affiliate_winklinks/__init__.py
+++ wink-sdk-affiliate-winklinks/wink_sdk_affiliate_winklinks/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # import apis into sdk package
 from wink_sdk_affiliate_winklinks.api.syndication_publisher_api import SyndicationPublisherApi
--- wink-sdk-user-settings/wink_sdk_user_settings/__init__.py
+++ wink-sdk-user-settings/wink_sdk_user_settings/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # import apis into sdk package
 from wink_sdk_user_settings.api.application_api import ApplicationApi
