b3df90f9c2979d556efb4f0fa3a28efcca886baa
flowmode <17271740+flowmode@users.noreply.github.com>
2025-04-06 15:55:05 +0700

    chore: remove pip upgrades

--- pypi-build.sh
+++ pypi-build.sh
@@ -98,9 +98,6 @@ find_all_sdks() {
 # Main Script Execution
 ###############################################################################
 
-echo "Installing/updating build tools..."
-pip install --upgrade pip setuptools wheel twine build
-
 find_all_sdks
 
 # Iterate over each SDK subdirectory and build it
