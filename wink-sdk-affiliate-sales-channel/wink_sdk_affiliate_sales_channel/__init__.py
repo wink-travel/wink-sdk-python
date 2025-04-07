2c5227c807ee3ebe201de43b9a2bd180f5e303b1
bjornharvold <bjorn@harvold.com>
2025-04-07 13:18:52 +0700

    chore: update SDKs after generation (version 0.0.18)

--- wink-sdk-affiliate-sales-channel/wink_sdk_affiliate_sales_channel/__init__.py
+++ wink-sdk-affiliate-sales-channel/wink_sdk_affiliate_sales_channel/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 
 # import apis into sdk package
 from wink_sdk_affiliate_sales_channel.api.available_supplier_api import AvailableSupplierApi
