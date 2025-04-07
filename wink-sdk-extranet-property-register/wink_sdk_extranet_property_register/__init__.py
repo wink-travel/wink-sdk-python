a01b889ecb86229a3214575aa5fb8c6944e31cd3
bjornharvold <bjorn@harvold.com>
2025-04-07 12:20:58 +0700

    chore: update SDKs after generation (version 0.0.15)

--- wink-sdk-extranet-property-register/wink_sdk_extranet_property_register/__init__.py
+++ wink-sdk-extranet-property-register/wink_sdk_extranet_property_register/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 
 # import apis into sdk package
 from wink_sdk_extranet_property_register.api.manage_leads_api import ManageLeadsApi
--- wink-sdk-ping/wink_sdk_ping/__init__.py
+++ wink-sdk-ping/wink_sdk_ping/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 
 # import apis into sdk package
 from wink_sdk_ping.api.ping_api import PingApi
